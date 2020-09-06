b9e361aff571e5cd6f5184d1e9cc4642523897b6
Ange <angepop1998@gmail.com>
2020-09-06 06:44:31 +0000

    Add function to application rbuplaod folder, add gitignore components

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,9 @@ module InstaClone
   class Application < Rails::Application  
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    I18n.config.enforce_available_locales = true
-    I18n.config.available_locales = :ja
-    I18n.config.default_locale = :ja
+    #I18n.config.enforce_available_locales = true
+    #I18n.config.available_locales = :ja
+    #I18n.config.default_locale = :ja
     
     config.load_defaults 5.2
     config.generators do |g|

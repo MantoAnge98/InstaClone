ff209ffa00f02bfbaf3105bf501fecd396895c47
ange <koutikaangemarie@gmail.com>
2020-09-06 08:09:13 +0100

    add jquery to assests pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.min.js
+//= require jquery 
+//= require jquery.turbolinks 
+//= require jquery_ujs 
 //= require_tree .

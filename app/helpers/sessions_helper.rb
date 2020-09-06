15c97d0de8ca5309fdaeaf6055ab69adda59f3b6
Ange <angepop1998@gmail.com>
2020-09-06 05:55:09 +0000

    Added sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,15 @@
 module SessionsHelper
+  #Créer une méthode d'assistance pour afficher la session
+    #de l'user connecté 
+    def current_user
+      #Si @current_user est faux lui attribuer ceci. 
+      @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  
+  #Définir une Méthode qui renvoie True si l'utilisateur 
+  #est connecté
+  def logged_in?
+      current_user.present?
+  end
 end

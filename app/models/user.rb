891fb787a3620eee2abb424db78b675786465bc4
Ange <angepop1998@gmail.com>
2020-09-06 06:00:37 +0000

    Added function to model, user, blog and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  has_many :blogs
+  has_many :favorites, dependent: :destroy 
+  mount_uploader :image, ImageUploader
+  
+  validates :image, presence: true
+  validates :name, presence: true, uniqueness: true, length: {maximum: 30}
+  before_validation {email.downcase!}
+  validates :email, presence: true, length: {maximum: 255},
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+
+  def total_followers
+   0 
+  end
+
+  def total_following
+    0
+  end
 end

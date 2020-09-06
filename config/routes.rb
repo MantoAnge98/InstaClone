d6c4d499791e464b662adc736022f99dc63b88dd
Ange <angepop1998@gmail.com>
2020-09-06 06:15:07 +0000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/inbox" if Rails.env.development?
+  root to: "public#homepage"
+  resources :favorites, only: [:create, :destroy, :show]
+  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :sessions, only: [:new, :create, :show, :destroy]
+
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+
+   #routes to errors
+   get '404', :to => 'errors#not_found'
+   get '500', :to => 'errors#internal_error'
+
 end
